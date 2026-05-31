5eb6adc578c5a2caea115a27e289170ed2b247f7
farahos <farahugaas0@gmail.com>
2026-05-31 21:28:56 +0300

    fix vercel flutter deploy

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const appConfig = {
   appName: "DeynRaac",
   tagline: "App-ka deynta oo fudud oo casri ah",
   apk: {
-    version: "v1.0.0",
+    version: "v1.0.9",
     fileName: "app-release.apk",
     updatedAt: "10 Abriil 2026",
   },
